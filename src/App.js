a3de3e2225b3cd4195407354ef1c050fd55fd18c
tinfoil-knight <kunal99kundu@gmail.com>
2020-03-12 13:41:54 +0530

    Revenue Cards Added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const RevenueCard = (props) => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="card">
+        <div className="card-title">{props.title}</div>
+        <div className="card-stat"><span style={{ fontSize: '1.5rem' }}>$ </span>{props.stat}k</div>
+      </div>
+    </>
+  )
+}
+
+const PieCard = (props) => {
+
+  return (
+    <>
+      <div>{props.title}</div>
+      <div>{props.stat}</div>
+    </>
+  )
+}
+
+const PieCardBox = () => {
+  const purchase = 11
+  const checkout = 8
+  const abandon = 88
+
+  return (
+    <>
+    <PieCard title="Purchase Rate" stat={purchase} />
+    <PieCard title="Checkout Rate" stat={checkout} />
+    <PieCard title="Cart Abandon Rate" stat={abandon} />
+
+    </>
+  )
+}
+
+const App = () => {
+
+  return (
+    <>
+
+      <RevenueCard title="Total Revenue" stat="9.02" />
+      <RevenueCard title="Revenue from Amazon" stat="5.75" />
+      <RevenueCard title="Revenue from Ebay" stat="1.91" />
+      <RevenueCard title="Revenue from Etsy" stat="1.36" />
+      {/* <SmallCard title="Product Views" stat="2.67k views"/> */}
+    </>
+  )
 }
 
 export default App;

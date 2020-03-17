1090b66146af8719a477dd2e59e91f44cfb1fbe3
tinfoil-knight <kunal99kundu@gmail.com>
2020-03-18 02:07:55 +0530

    API Fetching enabled

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
 import './App.css';
+
 import { ResponsivePie } from '@nivo/pie'
 import { ResponsiveBar } from '@nivo/bar'
 import { ResponsiveBubble } from '@nivo/circle-packing'
+
+import axios from 'axios'
 
 const Box = (props) => {
   return (
@@ -90,7 +93,7 @@ const PieChart = (props) => {
 }
 
 const BarChart = ({ data }) => {
-  const dimensions  = {height: 350, width: 600}
+  const dimensions = { height: 350, width: 600 }
   return (
     <div style={{ height: dimensions.height, width: dimensions.width }}>
       <ResponsiveBar
@@ -122,7 +125,7 @@ const BarChart = ({ data }) => {
 }
 
 const BubbleChart = ({ data }) => {
-  const dimensions  = {height: 350, width: 600}
+  const dimensions = { height: 350, width: 600 }
   return (
     <div style={{ height: dimensions.height, width: dimensions.width }}>
       <ResponsiveBubble
@@ -141,74 +144,124 @@ const BubbleChart = ({ data }) => {
   )
 }
 
-const App = () => {
-  const barData = [
-    {
-      "store": "Etsy",
-      "value": 8,
-    },
-    {
-      "store": "Ebay",
-      "value": 12,
-    },
-    {
-      "store": "Amazon",
-      "value": 44
-    }
-  ]
+const DynamicBoxes = () => {
 
-  const bubbleData = {
-    "name": "chart",
-    "children": [
-      {
-        "name": "NW",
-        "loc": 4
-      },
-      {
-        "name": "SW",
-        "loc": 2
-      },
-      {
-        "name": "CR",
-        "loc": 3
-      },
-      {
-        "name": "SE",
-        "loc": 1
-      },
-      {
-        "name": "NE",
-        "loc": 5
-      }
-    ]
+  const baseUrl = 'https://sales-dashboard-react.herokuapp.com/db/'
+
+  const [period, setPeriod] = useState({})
+
+  const hook = () => {
+    const getData = async () => {
+      const { data } = await axios('https://sales-dashboard-react.herokuapp.com/db/2019/Jan')
+      setPeriod({
+        'responseData': data,
+      })
+
+    }
+
+    getData()
   }
 
-  return (
+  useEffect(hook, [])
+  const truthValue = period.responseData === undefined ? false : true
+  
+  return  !truthValue ? (<div>Loading...</div>):(
     <>
-    <nav>
-      <span>Dashboard</span>
-      <span>Sales Summary</span>
-      <span>Hey, you!</span>
-    </nav>
+      {console.log("boxes loaded")}
       <Box>
-        <RevenueCard title="Revenue from Amazon" stat="5.75" />
-        <RevenueCard title="Revenue from Ebay" stat="1.91" />
-        <RevenueCard title="Revenue from Etsy" stat="1.36" />
-        <RevenueCard title="Total Revenue" stat="9.02" />
+        <RevenueCard title="Revenue from Amazon" stat={(period.responseData.Revenues.AM / 1000).toFixed(2)} />
+        <RevenueCard title="Revenue from Ebay" stat={(period.responseData.Revenues.EB / 1000).toFixed(2)} />
+        <RevenueCard title="Revenue from Etsy" stat={(period.responseData.Revenues.ET / 1000).toFixed(2)} />
+        <RevenueCard title="Total Revenue" stat={(period.responseData.Revenues.total / 1000).toFixed(2)} />
       </Box>
       <Box>
-        <PieChart title="Purchase Rate" stat={11} color="hsl(216, 54%, 49%)" />
-        <PieChart title="Checkout Rate" stat={8} color="hsl(186, 53%, 51%)" />
-        <PieChart title="Cart Abandon Rate" stat={88} color="hsl(69, 83%, 84%)" />
+        <PieChart title="Purchase Rate" stat={period.responseData.Rates.purchase} color="hsl(216, 54%, 49%)" />
+        <PieChart title="Checkout Rate" stat={period.responseData.Rates.checkout} color="hsl(186, 53%, 51%)" />
+        <PieChart title="Cart Abandon Rate" stat={period.responseData.Rates.abandoned} color="hsl(69, 83%, 84%)" />
       </Box>
       <Box>
         <Card title="Orders Trend by Stores">
-          <BarChart data={barData} />
+          <BarChart data={
+            [
+              {
+                "store": "Etsy",
+                "value": period.responseData.OrdersByStore.ET
+              },
+              {
+                "store": "Ebay",
+                "value": period.responseData.OrdersByStore.EB
+              },
+              {
+                "store": "Amazon",
+                "value": period.responseData.OrdersByStore.AM
+              }
+            ]
+          } />
         </Card>
         <Card title="Orders Trend by Region">
-          <BubbleChart data={bubbleData} />
+          <BubbleChart data={
+            {
+              "name": "chart",
+              "children": [
+                {
+                  "name": "NW",
+                  "loc": period.responseData.OrdersByRegion.nw
+                },
+                {
+                  "name": "SW",
+                  "loc": period.responseData.OrdersByRegion.sw
+                },
+                {
+                  "name": "CR",
+                  "loc": period.responseData.OrdersByRegion.c
+                },
+                {
+                  "name": "SE",
+                  "loc": period.responseData.OrdersByRegion.se
+                },
+                {
+                  "name": "NE",
+                  "loc": period.responseData.OrdersByRegion.ne
+                }
+              ]
+            }
+          } />
         </Card>
       </Box>
+    </>
+  )
+}
+
+const App = () => {
+
+
+  // const periodSplit = period.split(" ")
+  // const requestUrl = baseUrl + periodSplit[1] + "/" + periodSplit[0]
+
+  return (
+    <>
+      <nav>
+        <span>
+          <select id="month">
+            <option value="Jan 2019">Jan 2019</option>
+            <option value="Dec 2018">Dec 2018</option>
+            <option value="Nov 2018">Nov 2018</option>
+            <option value="Oct 2018">Oct 2018</option>
+            <option value="Sep 2018">Sep 2018</option>
+            <option value="Aug 2018">Aug 2018</option>
+            <option value="July 2018">July 2018</option>
+            <option value="Jun 2018">Jun 2018</option>
+            <option value="May 2018">May 2018</option>
+            <option value="Apr 2018">Apr 2018</option>
+            <option value="Mar 2018">Mar 2018</option>
+            <option value="Feb 2018">Feb 2018</option>
+            <option value="Jan 2018">Jan 2018</option>
+          </select>
+        </span>
+        <span>Sales Summary</span>
+        <span>Hey, you!</span>
+      </nav>
+      <DynamicBoxes />
     </>
   )
 
